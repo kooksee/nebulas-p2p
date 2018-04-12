8b4f72139d85d9eacb1d214d712f105d780122f3
白云辉 <baiyunhui@yuanben.org>
2018-04-12 19:55:56 +0800

    ok

---
+++ dispatcher.go
@@ -0,0 +1,130 @@
+// Copyright (C) 2017 go-nebulas authors
+//
+// This file is part of the go-nebulas library.
+//
+// the go-nebulas library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// the go-nebulas library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+package net
+
+import (
+	"sync"
+	"time"
+
+	"github.com/hashicorp/golang-lru"
+)
+
+// Dispatcher a message dispatcher service.
+type Dispatcher struct {
+	subscribersMap     *sync.Map
+	quitCh             chan bool
+	receivedMessageCh  chan Message
+	dispatchedMessages *lru.Cache
+	filters            map[string]bool
+}
+
+// NewDispatcher create Dispatcher instance.
+func NewDispatcher() *Dispatcher {
+	dp := &Dispatcher{
+		subscribersMap:    new(sync.Map),
+		quitCh:            make(chan bool, 10),
+		receivedMessageCh: make(chan Message, 65536),
+		filters:           make(map[string]bool),
+	}
+
+	dp.dispatchedMessages, _ = lru.New(10240)
+
+	return dp
+}
+
+// Register register subscribers.
+func (dp *Dispatcher) Register(subscribers ...*Subscriber) {
+	for _, v := range subscribers {
+		mt := v.MessageType()
+		m, _ := dp.subscribersMap.LoadOrStore(mt, new(sync.Map))
+		m.(*sync.Map).Store(v, true)
+		dp.filters[mt] = v.DoFilter()
+	}
+}
+
+// Deregister deregister subscribers.
+func (dp *Dispatcher) Deregister(subscribers ...*Subscriber) {
+
+	for _, v := range subscribers {
+		mt := v.MessageType()
+		m, _ := dp.subscribersMap.Load(mt)
+		if m == nil {
+			continue
+		}
+		m.(*sync.Map).Delete(v)
+		delete(dp.filters, mt)
+	}
+}
+
+// Start start message dispatch goroutine.
+func (dp *Dispatcher) Start() {
+	go dp.loop()
+}
+
+func (dp *Dispatcher) loop() {
+
+	timerChan := time.NewTicker(time.Second).C
+	for {
+		select {
+		case <-timerChan:
+			// metricsDispatcherCached.Update(int64(len(dp.receivedMessageCh)))
+		case <-dp.quitCh:
+			return
+		case msg := <-dp.receivedMessageCh:
+			msgType := msg.MessageType()
+			/* 			logging.VLog().WithFields(logrus.Fields{
+				"msgType": msgType,
+			}).Debug("dispatcher received message") */
+
+			v, _ := dp.subscribersMap.Load(msgType)
+			m, _ := v.(*sync.Map)
+
+			m.Range(func(key, value interface{}) bool {
+				select {
+				case key.(*Subscriber).msgChan <- msg:
+				default:
+				}
+				// logging.VLog().WithFields(logrus.Fields{
+				// 	"msgType": msgType,
+				// }).Debug("succeed dispatcher received message")
+				return true
+			})
+		}
+	}
+}
+
+// Stop stop goroutine.
+func (dp *Dispatcher) Stop() {
+
+	dp.quitCh <- true
+}
+
+// PutMessage put new message to chan, then subscribers will be notified to process.
+func (dp *Dispatcher) PutMessage(msg Message) {
+	// it's a optimize strategy for message dispatch, according to https://github.com/nebulasio/go-nebulas/issues/50
+	hash := msg.Hash()
+	if dp.filters[msg.MessageType()] {
+		if exist, _ := dp.dispatchedMessages.ContainsOrAdd(hash, hash); exist == true {
+			// duplicated message, ignore.
+			return
+		}
+	}
+
+	dp.receivedMessageCh <- msg
+}
